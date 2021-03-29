915e045ef4d0d855aea64a2ee504a0efeb660ec6
nisar-makdum <abrarabir97@gmail.com>
2021-03-29 11:15:00 +0600

    some changes

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(cors());
 
 const port = 5000;
 
+app.get('/', (req, res) => {
+    res.send("Hello From Database, It's Working")
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
